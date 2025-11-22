0fc536d5885513f76daa5ec8e7f2b0b5216e3439
Nutz95 <hexman@free.fr>
2025-11-22 22:42:56 +0100

    Remove legacy IMUTasks files (physical delete)

--- ESP32/include/IMUTasks.h
+++
@@ -1,5 +0,0 @@
-// IMUTasks.h -- REMOVED
-#pragma once
-
-#error "IMUTasks.h was removed. Include \"SystemTasks.h\" and update code to use the new API."
-
--- ESP32/src/IMUTasks.cpp
+++
@@ -1,3 +0,0 @@
-// IMUTasks.cpp -- removed compatibility shim
-// This file intentionally left blank. Use SystemTasks.cpp/SystemTasks.h instead.
-
